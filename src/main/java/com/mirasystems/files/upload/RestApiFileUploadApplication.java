9552e53ace7ee79b151c54bb35f62404484696be
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-17 23:59:35 -0300

    Initial commit

---
+++ src/main/java/com/mirasystems/files/upload/RestApiFileUploadApplication.java
@@ -0,0 +1,12 @@
+package com.mirasystems.files.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiFileUploadApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mirasystems/rest_api_file_upload/RestApiFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirasystems.rest_api_file_upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
